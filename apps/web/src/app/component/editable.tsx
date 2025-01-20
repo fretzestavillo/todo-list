f7223ab8c53bca421c3bb1d35f74f037ea31c69c
fretzestavillo <estavillofretz@gmail.com>
2025-01-20 10:49:13 +0800

    data is now realtime

--- apps/web/src/app/component/editable.tsx
+++ apps/web/src/app/component/editable.tsx
@@ -8,6 +8,7 @@ export function EditableText() {
   const app = initializeApp(firebaseConfig);
   const db = getFirestore(app);
   const [fbdata, setfbdata] = useState<string[]>([]);
+  const typingTimeoutRef = useRef<NodeJS.Timeout | null>(null);
 
   useEffect(() => {
     const docRef = doc(db, 'todoList', 'todo1');
@@ -23,22 +24,40 @@ export function EditableText() {
     return () => {
       unsubscribe();
     };
-  }, []);
+  }, [db]);
 
   const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
-    const updatedData = event.target.value.split('\n'); // Split lines by new line
+    const updatedData = event.target.value.split('\n');
     setfbdata(updatedData);
+
+    if (typingTimeoutRef.current) {
+      clearTimeout(typingTimeoutRef.current);
+    }
+
+    typingTimeoutRef.current = setTimeout(() => {
+      saveDataToFirestore(updatedData);
+    }, 3000);
+  };
+
+  const saveDataToFirestore = async (data: string[]) => {
+    try {
+      const docRef = doc(db, 'todoList', 'todo1');
+      await updateDoc(docRef, {
+        list: data,
+      });
+      console.log('Data saved to Firestore');
+    } catch (error) {
+      console.error('Error saving data to Firestore:', error);
+    }
   };
 
   return (
-    <>
-      <div>
-        <textarea
-          onChange={handleChange} // Track changes
-          value={fbdata.join('\n')}
-          className="inputStyle"
-        />{' '}
-      </div>
-    </>
+    <div>
+      <textarea
+        onChange={handleChange}
+        value={fbdata.join('\n')}
+        className="inputStyle"
+      />
+    </div>
   );
 }
